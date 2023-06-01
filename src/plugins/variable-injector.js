b9e0a5f2858b6f809f9e1c35448d747e24d4cac4
Jack Kleeman <jackkleeman@gmail.com>
2023-06-01 15:15:22 +0100

    Obtain SDK version and tour version from variable
    Fixes #28

--- src/plugins/variable-injector.js
+++ src/plugins/variable-injector.js
@@ -1,11 +1,16 @@
 const plugin = (options) => {
+    // Replace all occurrences of VAR::varName with the value of varName
+    const replace = (str) => str.replace(/VAR::([A-Z_]+)/g, (match, varName) => {
+        return options.replacements[varName] || match;
+    });
     const transformer = async (ast) => {
         const {visit} = await import('unist-util-visit')
-        visit(ast, ['text', 'code'], (node) => {
-            // Replace all occurrences of VAR::varName with the value of varName
-            node.value = node.value.replace(/VAR::([A-Z_]+)/g, (match, varName) => {
-                return options.replacements[varName] || match;
-            });
+        visit(ast, ['text', 'code', 'inlineCode', 'link'], (node) => {
+            if (node.type === 'link') {
+                node.url = replace(node.url)
+            } else {
+                node.value = replace(node.value)
+            }
         });
     };
     return transformer;
