1813a699ff20629b1b94d3bff839581e27ef9482
Giselle van Dongen <giselle.vd@gmail.com>
2023-10-26 10:06:51 +0200

    Add Java SDK overview

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -149,7 +149,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ["protobuf", "log"],
+        additionalLanguages: ["protobuf", "log", "java"],
         magicComments: [
           // Remember to extend the default highlight class name as well!
           {
