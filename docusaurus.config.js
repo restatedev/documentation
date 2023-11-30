ae4bd18dd7b3a99be483ed22370dc54000219f6d
Jack Kleeman <jackkleeman@gmail.com>
2023-12-01 00:14:32 +0200

    Remove social card (#189)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,8 +78,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
       navbar: {
         title: "",
         logo: {
