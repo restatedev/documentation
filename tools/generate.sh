711cc6b28e9042cb8104c92449ce9d1c0f77b8b2
Ahmed Farghal <me@asoli.dev>
2024-01-11 17:39:06 +0000

    Update Meta to Admin and add missing sql tables
    As per title

--- tools/generate.sh
+++ tools/generate.sh
@@ -17,7 +17,7 @@ $SCRIPT_DIR/generate_errors_page.sh $RESTATE_PATH/crates/errors/src/error_codes
 pushd $RESTATE_PATH
 
 echo "Generate OpenAPI"
-cargo xtask generate-rest-api-doc > $DOCS_DIR/static/schemas/openapi-meta.json
+cargo xtask generate-rest-api-doc > $DOCS_DIR/static/schemas/openapi-admin.json
 
 echo "Generate config schema"
 cargo xtask generate-config-schema > $DOCS_DIR/static/schemas/config_schema.json
