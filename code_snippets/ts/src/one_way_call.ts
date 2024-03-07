59b2b98df78da6f343b61e7d389034cb7effe326
Giselle van Dongen <giselle.vd@gmail.com>
2024-03-07 12:46:40 +0100

    Switch tags to <start_here> and <end_here> and add some docs

--- code_snippets/ts/src/one_way_call.ts
+++ code_snippets/ts/src/one_way_call.ts
@@ -3,12 +3,12 @@ import {myGreeterApi, myGreeterApiType} from "./service_communication";
 
 const router = restate.router({
     greet: async (ctx: restate.Context, name: string) => {
-        // start_here
+        // <start_here>
         // option 1: use only types and supply service name separately
         ctx.send<myGreeterApiType>({path: "greeter"}).greet("Pete");
 
         // option 2: use full API spec
         ctx.send(myGreeterApi).greet("Pete");
-        // end_here
+        // <end_here>
     },
 })
