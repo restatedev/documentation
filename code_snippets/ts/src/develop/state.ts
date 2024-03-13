403086390edebc93bd4a0d86da18c12dd7001ce2
Giselle van Dongen <giselle.vd@gmail.com>
2024-03-13 11:06:39 +0100

    Move state code snippets

---
+++ code_snippets/ts/src/develop/state.ts
@@ -0,0 +1,29 @@
+import * as restate from "@restatedev/restate-sdk";
+
+const router = restate.keyedRouter({
+    greet: async (ctx: restate.KeyedContext, name: string) => {
+        // <start_statekeys>
+        const stateKeys = ctx.stateKeys();
+        // <end_statekeys>
+
+        // <start_get>
+        const myStringValue = await ctx.get<string>("my-key") ?? "my-default";
+        // <end_get>
+
+        // <start_get_nb>
+        const myNbValue = await ctx.get<number>("my-key") ?? 0;
+        // <end_get_nb>
+
+        // <start_set>
+        ctx.set("my-key", "my-new-value");
+        // <end_set>
+
+        // <start_clear>
+        ctx.clear("my-key");
+        // <end_clear>
+
+        // <start_clear_all>
+        ctx.clearAll();
+        // <end_clear_all>
+    },
+})
