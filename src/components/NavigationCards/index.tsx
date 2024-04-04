2d8c7d61834d056b59af9684eb8e78b616d01868
Giselle van Dongen <giselle.vd@gmail.com>
2024-04-04 15:39:46 +0200

    Update overview page

--- src/components/NavigationCards/index.tsx
+++ src/components/NavigationCards/index.tsx
@@ -13,10 +13,10 @@ type FeatureItem = {
 function NavigationCard({title, category, stepLabel, description}: FeatureItem) {
     return (
         <div className={clsx('col col--4')}>
-            <div className={styles.feature}>
+            <div className={(category === "first_time") ? styles.featureFirstTime : styles.featureFamiliar}>
                 <div className={(category === "first_time") ? styles.circleFirstTime : styles.circleFamiliar}>{stepLabel}</div>
                 <h4 className="margin--xs">{title}</h4>
-                <p className={styles.description}><i>{description}</i></p>
+                <p className={styles.description}>{description}</p>
             </div>
         </div>
     );
@@ -24,7 +24,7 @@ function NavigationCard({title, category, stepLabel, description}: FeatureItem) 
 
 export default function NavigationCards({ features }): JSX.Element {
     return (
-        <section className={styles.features}>
+        <section>
             <div className="container">
                 <div className="row">
                     {features.map((props, idx) => (
