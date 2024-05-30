12f4b3d45062bb136498f9e243acd34ead9e56b9
Giselle van Dongen <giselle.vd@gmail.com>
2024-05-30 15:33:59 +0200

    Add the invocation docs to the concepts for workflows

--- code_snippets/java/src/main/java/concepts/invocations/DelayedCalls.java
+++ code_snippets/java/src/main/java/concepts/invocations/DelayedCalls.java
@@ -3,6 +3,7 @@ package concepts.invocations;
 import dev.restate.sdk.Context;
 import dev.restate.sdk.annotation.Handler;
 import dev.restate.sdk.annotation.Service;
+import develop.MyWorkflowClient;
 
 import java.time.Duration;
 
@@ -12,27 +13,29 @@ public class DelayedCalls {
     // <start_delayed_call>
     @Handler
     public void myRestateHandler(Context ctx){
-        // focus(1:3)
+        // focus(1:11)
         GreeterServiceClient.fromContext(ctx)
                 .send(Duration.ofMillis(1000))
                 .greet("Hi");
 
-        // focus(1:3)
         GreetCounterObjectClient.fromContext(ctx, "Mary")
                 .send(Duration.ofMillis(1000))
                 .greet("Hi");
+
+        MyWorkflowClient.fromContext(ctx, "wf-id-1")
+                .send(Duration.ofMillis(1000))
+                .run("input");
     }
     // <end_delayed_call>
 
     // <start_delayed_call_java>
     public void myJavaHandler(Context ctx){
-        // focus(1:4)
+        // focus(1:15)
         String invocationId = GreeterServiceClient
                 .fromIngress("http://localhost:8080")
                 .send(Duration.ofMillis(1000))
                 .greet("Hi");
 
-        // focus(1:4)
         String invocationIdCount = GreetCounterObjectClient
                 .fromIngress("http://localhost:8080", "Mary")
                 .send(Duration.ofMillis(1000))
--- code_snippets/java/src/main/java/concepts/invocations/OneWayCalls.java
+++ code_snippets/java/src/main/java/concepts/invocations/OneWayCalls.java
@@ -3,6 +3,7 @@ package concepts.invocations;
 import dev.restate.sdk.Context;
 import dev.restate.sdk.annotation.Handler;
 import dev.restate.sdk.annotation.Service;
+import develop.MyWorkflowClient;
 
 @Service
 public class OneWayCalls {
@@ -10,31 +11,37 @@ public class OneWayCalls {
     // <start_one_way_call>
     @Handler
     public void myRestateHandler(Context ctx){
-        // focus(1:3)
+        // focus(1:11)
         GreeterServiceClient.fromContext(ctx)
-                .send()
-                .greet("Hi");
+            .send()
+            .greet("Hi");
 
-        // focus(1:3)
         GreetCounterObjectClient.fromContext(ctx, "Mary")
-                .send()
-                .greet("Hi");
+            .send()
+            .greet("Hi");
+
+        MyWorkflowClient.fromContext(ctx, "wf-id-1")
+            .send()
+            .run("input");
     }
     // <end_one_way_call>
 
     // <start_one_way_call_java>
     public void myJavaHandler(Context ctx){
-        // focus(1:4)
+        // focus(1:13)
         String invocationId = GreeterServiceClient
                 .fromIngress("http://localhost:8080")
                 .send()
                 .greet("Hi");
 
-        // focus(1:4)
         GreetCounterObjectClient
                 .fromIngress("http://localhost:8080", "Mary")
                 .send()
                 .greet("Hi");
+
+        MyWorkflowClient
+                .fromIngress("http://localhost:8080", "wf-id-1")
+                .submit("input");
     }
     // <end_one_way_call_java>
 }
