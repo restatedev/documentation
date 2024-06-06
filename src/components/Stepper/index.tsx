adfb78177975d4c697c1c6dfe0f11d5ca0712640
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-06 13:51:41 +0200

    Improve installation docs of CLI and server and improve links to it in the quickstart

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-function Step({title, stepLabel, children}): JSX.Element {
+function Step({end, title, stepLabel, children}): JSX.Element {
     return (
         <div className="container">
             <div className={clsx('row')}>
@@ -11,13 +11,16 @@ function Step({title, stepLabel, children}): JSX.Element {
                     <h6 className={styles.title}>{title}</h6>
                 </div>
             </div>
-            <div className={clsx('row')}>
-                <div className={clsx('col')}>
-                    <div className={styles.feature}>
-                        <p className={styles.description}>{children}</p>
+            {
+                (end) ? <p className={"padding-bottom--md"}></p> : <div className={clsx('row')}>
+                    <div className={clsx('col')}>
+                        <div className={styles.feature}>
+                            <p className={styles.description}>{children}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
+
         </div>
     );
 }
