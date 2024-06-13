9ff8ade53422708951fbb765846419fd0f6848f0
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-13 11:37:49 +0200

    Document Kotlin clients

---
+++ code_snippets/kotlin/src/main/kotlin/develop/clients/GreeterService.kt
@@ -0,0 +1,14 @@
+package develop.clients
+
+import dev.restate.sdk.annotation.Handler
+import dev.restate.sdk.annotation.Service
+import dev.restate.sdk.kotlin.Context
+
+@Service
+class GreeterService {
+    @Handler
+    suspend fun greet(ctx: Context, greeting: String): String {
+        return "Hello, $greeting!"
+    }
+}
+
