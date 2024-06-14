cf8526406412fe9c81b0bb419369190ec1d41793
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-14 12:22:59 +0200

    Fix marking

--- code_snippets/java/src/main/java/use_cases/SignupWorkflow.java
+++ code_snippets/java/src/main/java/use_cases/SignupWorkflow.java
@@ -26,32 +26,47 @@ public class SignupWorkflow {
     // <mark_1>
     @Workflow
     public boolean run(WorkflowContext ctx, User user){
+        // <mark_3>
         ctx.set(STAGE, "Creating user");
+        // </mark_3>
         // <mark_2>
         ctx.run(() -> createUserEntry(ctx.key(), user.getName()));
         // </mark_2>
 
+        // <mark_3>
         ctx.set(STAGE, "Email verification");
+        // </mark_3>
         // <mark_2>
         String secret = ctx.random().nextUUID().toString();
         ctx.run(() -> sendEmailWithLink(user.getEmail(), secret));
         // </mark_2>
 
+        // <mark_5>
         String clickSecret = ctx.promise(EMAIL_LINK).awaitable().await();
+        // </mark_5>
+        // <mark_7>
         if(!clickSecret.equals(secret)){
+            // <mark_3>
             ctx.set(STAGE, "Verification failed");
+            // </mark_3>
             throw new TerminalException("Wrong secret from email link");
         }
+        // <mark_3>
+        // </mark_7>
         ctx.set(STAGE, "User verified");
+        // </mark_3>
         return true;
     }
     // </mark_1>
 
+    // <mark_4>
     @Handler
     public String getStage(SharedWorkflowContext ctx) {
         return ctx.get(STAGE).orElse("Unknown");
     }
+    // </mark_4>
 
+    // <mark_6>
     @Handler
     public void approveEmail(SharedWorkflowContext ctx, String secret){
         ctx.promiseHandle(EMAIL_LINK).resolve(secret);
@@ -61,6 +76,7 @@ public class SignupWorkflow {
     public void rejectEmail(SharedWorkflowContext ctx){
         ctx.promiseHandle(EMAIL_LINK).reject("Abort verification");
     }
+    // </mark_6>
 
     public static void main(String[] args) {
         RestateHttpEndpointBuilder
