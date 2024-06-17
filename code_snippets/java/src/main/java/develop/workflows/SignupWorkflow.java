16aea24979dadfb35a658321f5ba1f981eea4278
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-17 14:39:59 +0200

    Fix scrollycoding of workflow Java SDK page

--- code_snippets/java/src/main/java/develop/workflows/SignupWorkflow.java
+++ code_snippets/java/src/main/java/develop/workflows/SignupWorkflow.java
@@ -19,6 +19,7 @@ public class SignupWorkflow {
     private static final StateKey<String> STATUS =
             StateKey.of("status", JsonSerdes.STRING);
 
+    // <mark_1>
     @Workflow
     public boolean run(WorkflowContext ctx, Email email) {
         String secret = ctx.random().nextUUID().toString();
@@ -28,28 +29,37 @@ public class SignupWorkflow {
                 () -> sendEmailWithLink(email, secret));
         ctx.set(STATUS, "Sent email");
 
+        // <mark_3>
         String clickSecret = ctx.promise(EMAIL_CLICKED)
                 .awaitable()
                 .await();
+        // </mark_3>
         ctx.set(STATUS, "Clicked email");
 
         return clickSecret.equals(secret);
     }
+    // </mark_1>
 
     @Shared
     public void click(SharedWorkflowContext ctx, String secret) {
+        // <mark_3>
         ctx.promiseHandle(EMAIL_CLICKED).resolve(secret);
+        // </mark_3>
     }
 
+    // <mark_2>
     @Shared
     public String getStatus(SharedWorkflowContext ctx) {
         return ctx.get(STATUS).orElse("Unknown");
     }
+    // </mark_2>
 
     public static void main(String[] args) {
+        // <mark_4>
         RestateHttpEndpointBuilder.builder()
                 .bind(new SignupWorkflow())
                 .buildAndListen();
+        // </mark_4>
     }
 }
 // <end_here>
