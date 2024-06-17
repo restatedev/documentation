f9d3ad2cac1bdc4493ac686d5f630d3e5f7397cc
Giselle van Dongen <giselle.vd@gmail.com>
2024-06-17 15:48:30 +0200

    Fix scrollycoding workflows use cases page

--- code_snippets/ts/src/use_cases/signup_workflow.ts
+++ code_snippets/ts/src/use_cases/signup_workflow.ts
@@ -2,37 +2,57 @@ import * as restate from "@restatedev/restate-sdk";
 import {TerminalError, WorkflowContext, WorkflowSharedContext} from "@restatedev/restate-sdk";
 
 // <start_here>
+// <mark_1>
 const signUpWorkflow = restate.workflow({
     name: "sign-up-workflow",
     handlers: {
         run: async (ctx: WorkflowContext, user: User) => {
+            // </mark_1>
             const { id, name, email } = user;
 
+            // <mark_3>
             ctx.set("stage", "Creating User");
+            // </mark_3>
+            // <mark_2>
             await ctx.run(() => createUserEntry({ id, name }));
+            // </mark_2>
 
+            // <mark_3>
             ctx.set("stage", "Email Verification");
+            // </mark_3>
+            // <mark_2>
             const secret = ctx.rand.uuidv4();
             await ctx.run(() => sendEmailWithLink({ email, secret }));
+            // </mark_2>
 
+            // <mark_5>
             const clickSecret = await ctx.promise<string>("email-link");
+            // </mark_5>
+            // <mark_7>
             if (clickSecret !== secret) {
+                // <mark_3>
                 ctx.set("stage", `Verification failed`);
+                // </mark_3>
                 throw new TerminalError("Wrong secret from email link");
             }
+            // <mark_3>
             ctx.set("stage", "User verified");
+            // </mark_3>
             return true;
+            // </mark_7>
         },
 
+        // <mark_4>
         getStage: (ctx: WorkflowSharedContext) => ctx.get("stage"),
+        // </mark_4>
 
-        approveEmail: async (ctx: WorkflowSharedContext, secret: string) => {
-            await ctx.promise<string>("email-link").resolve(secret);
-        },
+        // <mark_6>
+        approveEmail: (ctx: WorkflowSharedContext, secret: string) =>
+            ctx.promise<string>("email-link").resolve(secret),
 
-        rejectEmail: async (ctx: WorkflowSharedContext) => {
-            await ctx.promise<string>("email-link").reject("Abort verification");
-        },
+        rejectEmail: (ctx: WorkflowSharedContext) =>
+            ctx.promise<string>("email-link").reject("Abort verification"),
+        // </mark_6>
     }
 });
 // <end_here>
