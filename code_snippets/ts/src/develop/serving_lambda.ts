9852a6fbef64d2dbe6fdbb8bb0cbf2fd0c2a479c
Jack Kleeman <jackkleeman@gmail.com>
2024-06-22 18:07:12 +0200

    Use new lambda handler API in docs

---
+++ code_snippets/ts/src/develop/serving_lambda.ts
@@ -0,0 +1,25 @@
+const myService = restate.service({
+  name: "MyService",
+  handlers: {},
+});
+
+const myVirtualObject = restate.object({
+  name: "MyVirtualObject",
+  handlers: {},
+});
+
+const myWorkflow = restate.workflow({
+  name: "MyWorkflow",
+  handlers: { run: async () => {} },
+});
+
+// <start_lambda>
+import * as restate from "@restatedev/restate-sdk/lambda";
+export const handler = restate
+  .endpoint()
+  .bind(myService)
+  .bind(myVirtualObject)
+  .bind(myWorkflow)
+  // withClass highlight-line
+  .handler();
+// <end_lambda>
