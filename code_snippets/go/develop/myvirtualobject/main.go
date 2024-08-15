e3ec6a412bf335e43d67eceabd282d30096bb638
Jack Kleeman <jackkleeman@gmail.com>
2024-08-15 15:01:31 +0100

    Use main() in service and virtual object

--- code_snippets/go/develop/myvirtualobject/main.go
+++ code_snippets/go/develop/myvirtualobject/main.go
@@ -1,8 +1,9 @@
-package develop
+package main
 
 import (
 	"context"
 	"fmt"
+	"log"
 
 	restate "github.com/restatedev/sdk-go"
 	server "github.com/restatedev/sdk-go/server"
@@ -18,8 +19,10 @@ func (MyVirtualObject) MyConcurrentHandler(ctx restate.ObjectSharedContext, gree
 	return fmt.Sprintf("%s %s!", greeting, ctx.Key()), nil
 }
 
-func serveMyVirtualObject() error {
-	return server.NewRestate().
+func main() {
+	if err := server.NewRestate().
 		Bind(restate.Reflect(MyVirtualObject{})).
-		Start(context.Background(), "0.0.0.0:9080")
+		Start(context.Background(), "0.0.0.0:9080"); err != nil {
+		log.Fatal(err)
+	}
 }
