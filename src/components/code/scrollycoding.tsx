11b20581d3b1964c3ef91769146e44ebf1707d01
Giselle van Dongen <giselle.vd@gmail.com>
2024-09-16 12:51:45 +0200

    Fix scrollycoding for tabs and steps

--- src/components/code/scrollycoding.tsx
+++ src/components/code/scrollycoding.tsx
@@ -1,49 +1,57 @@
 import { z } from "zod"
 import {
-  Selection,
-  Selectable,
-  SelectionProvider,
+    Selection,
+    Selectable,
+    SelectionProvider,
 } from "codehike/utils/selection"
 import { Block, HighlightedCodeBlock, parseProps } from "codehike/blocks"
 import styles from "./scrollycoding.module.css"
 import React from "react"
-import {Code, extractFlags} from "./code";
+import { Code, CodeTabs, extractFlags } from "./code"
 
 const Schema = Block.extend({
-  steps: z.array(
-    Block.extend({
-      code: HighlightedCodeBlock,
-    })
-  ),
+    steps: z.array(
+        Block.extend({
+            code: z.optional(HighlightedCodeBlock),
+            tabs: z.optional(z.array(HighlightedCodeBlock)),
+        })
+    ),
 })
 
 export function Scrollycoding(props: unknown) {
-  const { steps } = parseProps(props, Schema)
-  return (
-    <SelectionProvider className={styles.container}>
-      <div className={styles.stepsContainer}>
-        {steps.map((step, i) => (
-          <Selectable
-            key={i}
-            index={i}
-            selectOn={["click", "scroll"]}
-            className={styles.step}
-          >
-            <h2 className={styles.stepTitle}>{step.title}</h2>
-            <div>{step.children}</div>
-          </Selectable>
-        ))}
-      </div>
-      <Selection
-        from={steps.map((step) => (
-            <div className={styles.sticker}>
-                <div className={styles.stickyCode}>
-                    <Code codeblock={step.code}
-                        style={{ minHeight: "20rem", maxHeight:"50rem"}}/>
-                </div>
+    const { steps } = parseProps(props, Schema)
+
+    return (
+        <SelectionProvider className={styles.container}>
+            <div className={styles.stepsContainer}>
+                {steps.map((step, i) => (
+                    <Selectable
+                        key={i}
+                        index={i}
+                        selectOn={["click", "scroll"]}
+                        className={styles.step}
+                    >
+                        <h2 className={styles.stepTitle}>{step.title}</h2>
+                        <div>{step.children}</div>
+                    </Selectable>
+                ))}
             </div>
-        ))}
-      />
-    </SelectionProvider>
-  )
+            <Selection
+                from={steps.map((step) => (
+                    <div className={styles.sticker}>
+                        <div className={styles.stickyCode}>
+                            {"code" in step ? (
+                                <Code
+                                    codeblock={step.code}
+                                    style={{ minHeight: "20rem", maxHeight: "50rem" }}
+                                />
+                            ) : (
+                                <CodeTabs tabs={step.tabs} />
+                            )}
+                        </div>
+                    </div>
+                ))}
+            />
+        </SelectionProvider>
+    )
 }
