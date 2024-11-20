484d9967030735c6df50b451c5d2227334f41e64
slinkydeveloper <francescoguard@gmail.com>
2024-11-20 14:34:05 +0100

    Update Java SDK testing docs

---
+++ code_snippets/java/src/main/java/develop/GreeterTestMethod.java
@@ -0,0 +1,29 @@
+package develop;
+
+import dev.restate.sdk.client.Client;
+import dev.restate.sdk.testing.BindService;
+import dev.restate.sdk.testing.RestateClient;
+import dev.restate.sdk.testing.RestateTest;
+import develop.clients.GreeterService;
+import develop.clients.GreeterServiceClient;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RestateTest
+class GreeterTestMethod {
+
+  @BindService GreeterService service = new GreeterService();
+
+  // <start_test>
+  @Test
+  void testGreet(@RestateClient Client ingressClient) {
+    // Create the service client from the injected ingress client
+    var client = GreeterServiceClient.fromClient(ingressClient);
+
+    // Send request to service and assert the response
+    var response = client.greet("Francesco");
+    assertEquals(response, "Hello, Francesco!");
+  }
+  // <end_test>
+}
