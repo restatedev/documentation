aa51260b48fa8ac7e0a39f60633a20153d08ca5d
Francesco Guardiani <francescoguard@gmail.com>
2024-11-20 15:06:33 +0100

    Java sdk 1.2 updates (#481)
    * Use version catalog to manage java/kotlin dependencies
    
    * Update Java SDK testing docs
    
    * Javadocs/ktdocs update
    
    * Spotless

--- code_snippets/java/src/main/java/develop/GreeterTest.java
+++ code_snippets/java/src/main/java/develop/GreeterTest.java
@@ -1,37 +1,15 @@
 package develop;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import dev.restate.sdk.client.Client;
-import dev.restate.sdk.testing.RestateClient;
-import dev.restate.sdk.testing.RestateRunner;
-import dev.restate.sdk.testing.RestateRunnerBuilder;
+import dev.restate.sdk.testing.*;
 import develop.clients.GreeterService;
-import develop.clients.GreeterServiceClient;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
+// <start_extension>
+@RestateTest
 class GreeterTest {
 
-  // <start_extension>
-  @RegisterExtension
-  private static final RestateRunner RESTATE_RUNNER =
-      RestateRunnerBuilder.create()
-          // The service to test
-          .bind(new GreeterService())
-          .buildRunner();
+  @BindService GreeterService service = new GreeterService();
 
-  // <end_extension>
+  // Your tests
 
-  // <start_test>
-  @Test
-  void testGreet(@RestateClient Client ingressClient) {
-    // Create the service client from the injected ingress client
-    var client = GreeterServiceClient.fromClient(ingressClient);
-
-    // Send request to service and assert the response
-    var response = client.greet("Francesco");
-    assertEquals(response, "Hello, Francesco!");
-  }
-  // <end_test>
 }
+// <end_extension>
---
+++ code_snippets/kotlin/src/main/kotlin/develop/GreeterTestMethod.kt
@@ -0,0 +1,29 @@
+package develop
+
+import dev.restate.sdk.client.Client
+import dev.restate.sdk.testing.BindService
+import dev.restate.sdk.testing.RestateClient
+import dev.restate.sdk.testing.RestateTest
+import develop.clients.GreeterService
+import develop.clients.GreeterServiceClient
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@RestateTest
+class GreeterTestMethod {
+
+  @BindService val service = GreeterService()
+
+  // <start_test>
+  @Test
+  fun testGreet(@RestateClient ingressClient: Client) = runTest {
+    // Create the service client from the injected ingress client
+    val client = GreeterServiceClient.fromClient(ingressClient)
+
+    // Send request to service and assert the response
+    val response = client.greet("Francesco")
+    assertEquals(response, "Hello, Francesco!")
+  }
+  // <end_test>
+}
