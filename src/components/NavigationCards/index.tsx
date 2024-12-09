55e2e0824172f12482d20451a71d6927f5d83c2b
Giselle van Dongen <giselle.vd@gmail.com>
2024-12-09 15:31:01 +0100

    Redo overview page

--- src/components/NavigationCards/index.tsx
+++ src/components/NavigationCards/index.tsx
@@ -7,26 +7,41 @@ type FeatureItem = {
     category: string
     stepLabel: string;
     description: JSX.Element;
+    link?: string;
 };
 
 
-function NavigationCard({title, category, stepLabel, description}: FeatureItem) {
-    return (
+function NavigationCard({title, category, stepLabel, description, link}: FeatureItem) {
+    const cardContent = (
             <div className={(category === "first_time") ? styles.featureFirstTime : styles.featureFamiliar}>
                 <div className={(category === "first_time") ? styles.circleFirstTime : styles.circleFamiliar}>{stepLabel}</div>
                 <h6 className={styles.title}>{title}</h6>
                 <div className={styles.description}>{description}</div>
             </div>
+        );
+
+    return (
+        link ? (
+            <a href={link} style={{textDecoration: "none"}} >
+                {cardContent}
+            </a>
+        ) : (
+            <>
+                {cardContent}
+            </>
+        )
     );
 }
 
 export default function NavigationCards({ features }): JSX.Element {
+
+    const colSize = 12 / features.length;
     return (
         <section>
             <div className="container">
                 <div className="row">
                         {features.map((props, idx) => (
-                            <div className={clsx('col col--3 margin-vert--md')} key={props.title}>
+                            <div className={clsx(`col col--${colSize} margin-vert--md`)} key={props.title}>
                                 <NavigationCard key={idx} {...props} />
                             </div>
                         ))}
