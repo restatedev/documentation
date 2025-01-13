6e50e97e1517111058407c79276e63e67d589a42
Giselle van Dongen <giselle.vd@gmail.com>
2025-01-13 11:09:53 +0100

    Collapse comments/imports and improve sagas guides

--- src/plugins/code-loader.js
+++ src/plugins/code-loader.js
@@ -1,15 +1,36 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-const COMMENT_SYMBOL = {
-    ts: "//",
-    java: "//",
-    kotlin: "//",
-    python: "#",
-    go: "//",
-    proto: "//",
-    rust: "//"
-}
+const LANGUAGE_SYMBOLS = {
+    ts: {
+        commentSymbol: "//",
+        serviceSymbol: ["restate.service", "restate.object", "restate.workflow"]
+    },
+    java: {
+        commentSymbol: "//",
+        serviceSymbol: ["@Service", "@VirtualObject", "@Workflow"]
+    },
+    kotlin: {
+        commentSymbol: "//",
+        serviceSymbol: ["@Service", "@VirtualObject", "@Workflow"]
+    },
+    python: {
+        commentSymbol: "#",
+        serviceSymbol: ["= VirtualObject(", "= Service(", "= Workflow("]
+    },
+    go: {
+        commentSymbol: "//",
+        serviceSymbol: ["struct{}"]
+    },
+    proto: {
+        commentSymbol: "//",
+        serviceSymbol: []
+    },
+    rust: {
+        commentSymbol: "//",
+        serviceSymbol: ["#[restate_sdk::service]", "#[restate_sdk::object]", "#[restate_sdk::workflow]"]
+    }
+};
 
 const plugin = (options) => {
     const codeLoadRegex = /.*CODE_LOAD::([^#?]+)(?:#([^?]*))?(?:\?(.+))?$/g;
@@ -25,9 +46,13 @@ const plugin = (options) => {
             return str;
         }
 
+        // Take care of the collapse_prequel tag (we then need to collapse all imports before the first restate object)
+        const doCollapsePrequel = str.includes("collapse_prequel")
+        const cleanedStr = str.split("\n").filter(line => !line.includes("collapse_prequel")).join("\n")
+
         const fileContent = await readFileOrFetch(fileData.filePath);
-        const data = extractAndClean(fileContent, fileData.customTag, fileData.markNumber, fileData.filePath);
-        return str.replace(codeLoadRegex, (match) => match.replace(/.*CODE_LOAD::[^#?]+(?:#([^?]*))?(?:\?(.+))?$/, data));
+        const data = extractAndClean(fileContent, fileData.customTag, fileData.markNumber, fileData.filePath, doCollapsePrequel);
+        return cleanedStr.replace(codeLoadRegex, (match) => match.replace(/.*CODE_LOAD::[^#?]+(?:#([^?]*))?(?:\?(.+))?$/, data));
     };
 
     async function readFileOrFetch(filepath) {
@@ -42,28 +67,28 @@ const plugin = (options) => {
         }
     }
 
-    function extractCommentSymbol(filePath) {
+    function extractLanguageSymbol(filePath) {
         if (filePath.includes(".java")) {
-            return COMMENT_SYMBOL.java;
+            return LANGUAGE_SYMBOLS.java;
         } else if (filePath.includes(".kt")) {
-            return COMMENT_SYMBOL.kotlin;
+            return LANGUAGE_SYMBOLS.kotlin;
         } else if (filePath.includes(".ts")) {
-            return COMMENT_SYMBOL.ts;
+            return LANGUAGE_SYMBOLS.ts;
         } else if (filePath.includes(".py")) {
-            return COMMENT_SYMBOL.python;
+            return LANGUAGE_SYMBOLS.python;
         } else if (filePath.includes(".go")) {
-            return COMMENT_SYMBOL.go;
+            return LANGUAGE_SYMBOLS.go;
         } else if (filePath.includes(".rs")) {
-            return COMMENT_SYMBOL.rust;
+            return LANGUAGE_SYMBOLS.rust;
         } else if (filePath.includes(".proto")) {
-            return COMMENT_SYMBOL.proto;
+            return LANGUAGE_SYMBOLS.proto;
         } else {
             throw new Error(`language not detected for filepath ${filePath}`)
         }
     }
 
-    function extractAndClean(fileContent, customTag, markNumber, filePath) {
-        const commentSymbol = extractCommentSymbol(filePath)
+    function extractAndClean(fileContent, customTag, markNumber, filePath, doCollapsePrequel) {
+        const {commentSymbol, serviceSymbol} = extractLanguageSymbol(filePath)
 
         const startTag = (customTag) ? `<start_${customTag}>` : "<start_here>";
         const endTag = (customTag) ? `<end_${customTag}>` : "<end_here>";
@@ -86,7 +111,7 @@ const plugin = (options) => {
                 .filter(line => !line.includes(`${commentSymbol} break`));
         }
 
-        let finalLines = [];
+        let filteredLines = [];
         if (markNumber) {
             const markStartTag = `${commentSymbol} <mark_${markNumber}>`;
             const markEndTag = `${commentSymbol} </mark_${markNumber}>`;
@@ -108,26 +133,54 @@ const plugin = (options) => {
 
                 if(!line.includes('<start_') && !line.includes('<end_') && !line.includes('<mark_') && !line.includes('</mark_')) {
                     if(needToMark){
-                        finalLines.push(`${commentSymbol} !mark`)
+                        filteredLines.push(`${commentSymbol} !mark`)
                     }
-                    finalLines.push(line);
+                    filteredLines.push(line);
                 }
             });
         } else {
-            finalLines = lines;
+            filteredLines = lines;
         }
 
         const leadingWhitespace = lines[0].match(/^\s*/)[0];
 
-        return finalLines.filter(line => {
+        let finalLines = filteredLines.filter(line => {
             // filter out all code loader tags
                 return !line.includes('<start_') &&
                     !line.includes('<end_') &&
                     !line.includes('<mark_') &&
-                    !line.includes('</mark_');
+                    !line.includes('</mark_') &&
+                    !line.includes('collapse_prequel');
             })
+
+        if (doCollapsePrequel) {
+            console.log(`Collapsing prequel`)
+            finalLines = collapsePrequel(finalLines, serviceSymbol, commentSymbol);
+        }
+
+        return finalLines
             .map(line => line.replace(new RegExp(`^${leadingWhitespace}`), ''))
             .join('\n');
+    }
+
+    function collapsePrequel(lines, serviceSymbol, commentSymbol) {
+        let collapseIndex = -1;
+        for (let i = 0; i < lines.length; i++) {
+            if (serviceSymbol.some(symbol => lines[i].includes(symbol))) {
+                collapseIndex = i;
+                break;
+            }
+        }
+
+        if (collapseIndex !== -1) {
+            const collapseComment = `${commentSymbol} !collapse(1:${collapseIndex + 1}) collapsed \n ${commentSymbol} Click to expand imports/comments...`
+            // Add collapseComment as first line to lines
+            let collapsedContent = [collapseComment, ...lines]
+
+            return collapsedContent;
+        } else {
+            throw new Error('No service/object/workflow found in file, so cannot collapse prequel');
+        }
     }
 
     const transformer = async (ast) => {
