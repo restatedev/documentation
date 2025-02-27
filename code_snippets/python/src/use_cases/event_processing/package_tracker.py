de35cbaf48ea00375fb85615ae81458030517149
Giselle van Dongen <giselle.vd@gmail.com>
2025-02-27 12:44:31 +0100

    Add event processing python use case page

---
+++ code_snippets/python/src/use_cases/event_processing/package_tracker.py
@@ -0,0 +1,69 @@
+from typing import List
+
+import restate
+from pydantic import BaseModel
+from restate import VirtualObject, ObjectContext
+from restate.exceptions import TerminalError
+
+
+class LocationUpdate(BaseModel):
+    location: str
+    timestamp: str
+
+
+class PackageInfo(BaseModel):
+    final_destination: str
+    locations: List[LocationUpdate] = []
+
+
+# <start_here>
+# <mark_2>
+package_tracker = VirtualObject("package-tracker")
+# </mark_2>
+
+
+# <mark_3>
+# <mark_2>
+@package_tracker.handler()
+async def register_package(ctx: ObjectContext, package_info: PackageInfo):
+    # </mark_2>
+    # </mark_3>
+    # <mark_1>
+    ctx.set("package-info", package_info.model_dump())
+    # </mark_1>
+
+
+# <mark_3>
+# <mark_2>
+@package_tracker.handler()
+async def update_location(ctx: ObjectContext, location_update: LocationUpdate):
+    # </mark_2>
+    # </mark_3>
+    # <mark_1>
+    package_info = PackageInfo(**await ctx.get("package-info"))
+    # </mark_1>
+    if package_info is None:
+        raise TerminalError(f"Package {ctx.key()} not found")
+
+    locations = package_info.locations or []
+    locations.append(location_update)
+    package_info.locations = locations
+
+    # <mark_1>
+    ctx.set("package-info", package_info.model_dump())
+    # </mark_1>
+
+
+# <mark_3>
+# <mark_2>
+@package_tracker.handler()
+async def get_package_info(ctx: ObjectContext) -> PackageInfo:
+    # </mark_2>
+    # </mark_3>
+    # <mark_1>
+    return PackageInfo(**await ctx.get("package-info"))
+    # </mark_1>
+
+
+app = restate.app(services=[package_tracker])
+# <end_here>
