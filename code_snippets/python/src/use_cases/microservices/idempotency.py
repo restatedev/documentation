2eda607711bb2fed56a649b30e1d1cfe06d91467
Giselle van Dongen <giselle.vd@gmail.com>
2025-03-07 09:20:50 +0100

    Fix idempotency error

--- code_snippets/python/src/use_cases/microservices/idempotency.py
+++ code_snippets/python/src/use_cases/microservices/idempotency.py
@@ -3,7 +3,7 @@ import requests
 from src.use_cases.microservices.subscription_service import SubscriptionRequest
 
 RESTATE_URL = "http://localhost:8080"
-subscription_request = SubscriptionRequest("user123", "1234", ["sub1", "sub2"])
+subscription_request = SubscriptionRequest(user_id="user123", credit_card="1234", subscriptions=["sub1", "sub2"])
 request_id = "myTask123"
 # <start_here>
 # <mark_1>
