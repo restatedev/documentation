98c667e53cc38f326bdb2069e054a8c35e29674e
Giselle van Dongen <giselle.vd@gmail.com>
2025-03-19 14:38:48 +0100

    Clean up tabs on invocations page and add UI screenshot

--- code_snippets/java/src/main/java/concepts/invocations/DelayedCalls.java
+++ code_snippets/java/src/main/java/concepts/invocations/DelayedCalls.java
@@ -12,26 +12,20 @@ public class DelayedCalls {
   // <start_delayed_call>
   @Handler
   public void myRestateHandler(Context ctx) {
-    // !focus
-    GreeterServiceClient.fromContext(ctx)
-        // !focus
-        .send(Duration.ofMillis(1000))
-        // !focus
-        .greet("Hi");
+    // !focus(1:3)
+    // From a Java Restate service
+    Duration delay = Duration.ofDays(5);
+    GreeterServiceClient.fromContext(ctx).send(delay).greet("Hi");
   }
-
   // <end_delayed_call>
 
   // <start_delayed_call_java>
   public void myJavaHandler() {
-    // !focus
-    Client restate = Client.connect("http://localhost:8080");
-    // !focus
-    GreeterServiceClient.fromClient(restate)
-        // !focus
-        .send(Duration.ofMillis(1000))
-        // !focus
-        .greet("Hi");
+    // !focus(1:4)
+    // Or from any other Java program
+    Client restateClient = Client.connect("http://localhost:8080");
+    Duration delay = Duration.ofDays(5);
+    GreeterServiceClient.fromClient(restateClient).send(delay).greet("Hi");
   }
   // <end_delayed_call_java>
 }
--- code_snippets/java/src/main/java/concepts/invocations/OneWayCalls.java
+++ code_snippets/java/src/main/java/concepts/invocations/OneWayCalls.java
@@ -11,26 +11,18 @@ public class OneWayCalls {
   // <start_one_way_call>
   @Handler
   public void myRestateHandler(Context ctx) {
-    // !focus
-    GreeterServiceClient.fromContext(ctx)
-        // !focus
-        .send()
-        // !focus
-        .greet("Hi");
+    // !focus(1:2)
+    // From a Java Restate service
+    GreeterServiceClient.fromContext(ctx).send().greet("Hi");
   }
-
   // <end_one_way_call>
 
   // <start_one_way_call_java>
   public void myJavaHandler() {
-    // !focus
-    Client restate = Client.connect("http://localhost:8080");
-    // !focus
-    GreeterServiceClient.fromClient(restate)
-        // !focus
-        .send()
-        // !focus
-        .greet("Hi");
+    // !focus(1:3)
+    // Or from any other Java program
+    Client restateClient = Client.connect("http://localhost:8080");
+    GreeterServiceClient.fromClient(restateClient).send().greet("Hi");
   }
   // <end_one_way_call_java>
 }
