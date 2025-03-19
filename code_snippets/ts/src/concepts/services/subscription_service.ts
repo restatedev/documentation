c4ece5bcabd983677a9688fb9a9d431faef8084f
Giselle van Dongen <giselle.vd@gmail.com>
2025-03-19 18:01:37 +0100

    First updates to services page

---
+++ code_snippets/ts/src/concepts/services/subscription_service.ts
@@ -0,0 +1,48 @@
+import * as restate from "@restatedev/restate-sdk";
+
+// <start_here>
+// <mark_2>
+const subscriptionService = restate.service({
+  // </mark_2>
+  name: "SubscriptionService",
+  // <mark_2>
+  handlers: {
+    add: async (ctx: restate.Context, req: SubscriptionRequest) => {
+      // </mark_2>
+      // <mark_3>
+      // <mark_1>
+      const paymentId = ctx.rand.uuidv4();
+      // </mark_1>
+
+      // <mark_1>
+      const payRef = await ctx.run(() =>
+        createRecurringPayment(req.creditCard, paymentId)
+      );
+      // </mark_1>
+
+      for (const subscription of req.subscriptions) {
+        // <mark_1>
+        await ctx.run(() =>
+          createSubscription(req.userId, subscription, payRef)
+        );
+        // </mark_1>
+      }
+      // </mark_3>
+    },
+  },
+});
+
+restate.endpoint().bind(subscriptionService).listen(9080);
+// <end_here>
+
+type SubscriptionRequest = {
+  creditCard: string;
+  userId: any;
+  subscriptions: any[];
+};
+
+function createRecurringPayment(creditCard: string, paymentId: string) {}
+
+function createSubscription(userId: any, subscription: any, payRef: void) {
+  return undefined;
+}
--- code_snippets/ts/src/concepts/services/virtual_objects.ts
+++ code_snippets/ts/src/concepts/services/virtual_objects.ts
@@ -1,13 +1,11 @@
-/**
- * WARNING:
- * The Services page relies on the line numbers for the code animations
- * Make sure you adapt the line numbers when adapting the code
- */
+import * as restate from "@restatedev/restate-sdk";
+import {
+  handlers,
+  ObjectContext,
+  ObjectSharedContext,
+} from "@restatedev/restate-sdk";
 
 // <start_here>
-import * as restate from "@restatedev/restate-sdk";
-import { ObjectContext } from "@restatedev/restate-sdk";
-
 // <mark_1>
 export const greeterObject = restate.object({
   // </mark_1>
@@ -15,39 +13,45 @@ export const greeterObject = restate.object({
   handlers: {
     // <mark_1>
     // <mark_3>
-    greet: async (ctx: ObjectContext, greeting: string) => {
+    greet: async (ctx: restate.ObjectContext, req: { greeting: string }) => {
       // </mark_3>
+      const name = ctx.key;
       // </mark_1>
       // <mark_2>
       let count = (await ctx.get<number>("count")) ?? 0;
+      // </mark_2>
       count++;
+      // <mark_2>
       ctx.set("count", count);
       // </mark_2>
-      // <mark_1>
-      return `${greeting} ${ctx.key} for the ${count}-th time.`;
-      // </mark_1>
+      return `${req.greeting} ${name} for the ${count}-th time.`;
     },
 
     // <mark_1>
     // <mark_3>
-    ungreet: async (ctx: ObjectContext) => {
+    ungreet: async (ctx: restate.ObjectContext) => {
       // </mark_3>
+      const name = ctx.key;
       // </mark_1>
       // <mark_2>
       let count = (await ctx.get<number>("count")) ?? 0;
       // </mark_2>
       if (count > 0) {
-        // <mark_2>
         count--;
-        // </mark_2>
       }
       // <mark_2>
       ctx.set("count", count);
       // </mark_2>
-      // <mark_1>
-      return `Dear ${ctx.key}, taking one greeting back: ${count}.`;
-      // </mark_1>
+      return `Dear ${name}, taking one greeting back: ${count}.`;
     },
+
+    // <mark_4>
+    getGreetCount: handlers.object.shared(
+      async (ctx: restate.ObjectSharedContext) => {
+        return await ctx.get<number>("count");
+      }
+    ),
+    // </mark_4>
   },
 });
 
