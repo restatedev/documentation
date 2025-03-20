a8290176c124bf110704df6efbf10e7a2bfa2016
Giselle van Dongen <giselle.vd@gmail.com>
2025-03-20 11:58:09 +0100

    Add docs links to services page

--- src/components/SdkIcons/index.tsx
+++ src/components/SdkIcons/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./styles.module.css";
 
 type SdkLinks = {
+    description?: string;
     java?: string;
     ts?: string;
     kotlin?: string;
@@ -11,81 +12,82 @@ type SdkLinks = {
 };
 
 export default function SdkIcons({ sdkLinks }: { sdkLinks: SdkLinks }): JSX.Element {
-    const { java, ts, kotlin, python, go, rust } = sdkLinks;
+    const { description, java, ts, kotlin, python, go, rust } = sdkLinks;
     return (
-        <div>
-            <div className={styles.langContainer}>
-                {ts ? (
-                    <div id="overviewButtonDiv">
-                        <a
-                            className="overviewButton btn btn-primary btn-lg firstTimeButton"
-                            href={ts}
-                            role="button"
-                        >
-                            <img
-                                className="buttonIcon mb-0.5"
-                                src="/img/typescript.svg"
-                                width="28"
-                            />
-                        </a>
-                    </div>
-                ) : null}
-                {java ? (
-                    <div id="overviewButtonDiv">
-                        <a
-                            className="overviewButton btn btn-primary btn-lg firstTimeButton"
-                            href={java}
-                            role="button"
-                        >
-                            <img className="buttonIcon mb-0.5" src="/img/java.svg" width="28" />
-                        </a>
-                    </div>
-                ) : null}
-                {kotlin ? (
-                    <div id="overviewButtonDiv">
-                        <a
-                            className="overviewButton btn btn-primary btn-lg firstTimeButton"
-                            href={kotlin}
-                            role="button"
-                        >
-                            <img className="buttonIcon mb-0.5" src="/img/kotlin.svg" width="28" />
-                        </a>
-                    </div>
-                ) : null}
-                {go ? (
-                    <div id="overviewButtonDiv">
-                        <a
-                            className="overviewButton btn btn-primary btn-lg firstTimeButton"
-                            href={go}
-                            role="button"
-                        >
-                            <img className="buttonIcon mb-0.5" src="/img/go.svg" width="40" />
-                        </a>
-                    </div>
-                ) : null}
-                {python ? (
-                    <div id="overviewButtonDiv">
-                        <a
-                            className="overviewButton btn btn-primary btn-lg firstTimeButton"
-                            href={python}
-                            role="button"
-                        >
-                            <img className="buttonIcon mb-0.5" src="/img/python.svg" width="28" />
-                        </a>
-                    </div>
-                ) : null}
-                {rust ? (
-                    <div id="overviewButtonDiv">
-                        <a
-                            className="overviewButton btn btn-primary btn-lg firstTimeButton"
-                            href={rust}
-                            role="button"
-                        >
-                            <img className="buttonIcon mb-0.5" src="/img/rust.svg" width="28" />
-                        </a>
-                    </div>
-                ) : null}
-            </div>
+        <div className={styles.langContainer}>
+            {description ? (
+                <p>{description}</p>
+            ): null}
+            {ts ? (
+                <div id="overviewButtonDiv">
+                    <a
+                        className="overviewButton btn btn-primary btn-lg firstTimeButton"
+                        href={ts}
+                        role="button"
+                    >
+                        <img
+                            className="buttonIcon mb-0.5"
+                            src="/img/typescript.svg"
+                            width="24"
+                        />
+                    </a>
+                </div>
+            ) : null}
+            {java ? (
+                <div id="overviewButtonDiv">
+                    <a
+                        className="overviewButton btn btn-primary btn-lg firstTimeButton"
+                        href={java}
+                        role="button"
+                    >
+                        <img className="buttonIcon mb-0.5" src="/img/java.svg" width="24" />
+                    </a>
+                </div>
+            ) : null}
+            {kotlin ? (
+                <div id="overviewButtonDiv">
+                    <a
+                        className="overviewButton btn btn-primary btn-lg firstTimeButton"
+                        href={kotlin}
+                        role="button"
+                    >
+                        <img className="buttonIcon mb-0.5" src="/img/kotlin.svg" width="24" />
+                    </a>
+                </div>
+            ) : null}
+            {go ? (
+                <div id="overviewButtonDiv">
+                    <a
+                        className="overviewButton btn btn-primary btn-lg firstTimeButton"
+                        href={go}
+                        role="button"
+                    >
+                        <img className="buttonIcon mb-0.5" src="/img/go.svg" width="40" />
+                    </a>
+                </div>
+            ) : null}
+            {python ? (
+                <div id="overviewButtonDiv">
+                    <a
+                        className="overviewButton btn btn-primary btn-lg firstTimeButton"
+                        href={python}
+                        role="button"
+                    >
+                        <img className="buttonIcon mb-0.5" src="/img/python.svg" width="24" />
+                    </a>
+                </div>
+            ) : null}
+            {rust ? (
+                <div id="overviewButtonDiv">
+                    <a
+                        className="overviewButton btn btn-primary btn-lg firstTimeButton"
+                        href={rust}
+                        role="button"
+                    >
+                        <img className="buttonIcon mb-0.5" src="/img/rust.svg" width="24" />
+                    </a>
+                </div>
+            ) : null}
         </div>
     );
 }
