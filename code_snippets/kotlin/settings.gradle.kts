82b8e85c61c248c884264e5cab42d9e395df390a
Francesco Guardiani <francescoguard@gmail.com>
2025-04-10 14:26:28 +0200

    Fix kotlin/java version (#591)

--- code_snippets/kotlin/settings.gradle.kts
+++ code_snippets/kotlin/settings.gradle.kts
@@ -4,6 +4,6 @@ dependencyResolutionManagement {
   repositories {
     mavenCentral()
     // Needed for SDK snapshots, comment when using released versions.
-    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    // maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
   }
 }
